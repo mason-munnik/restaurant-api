b1793646081e5ddea67f264e5a518e7358eb143c
mmunnik <mason-munnik@uiowa.edu>
2026-03-10 10:14:40 -0500

    Revert "Made post endpoint commit to db and added get endpoint to view the db"
    This reverts commit 828c56ea68aac687ba6543d62f176e375b9a954a.

--- database.py
+++ database.py
@@ -13,3 +13,4 @@ engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False}, 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
